208dec95566e2b0771c6882d9222a9e63e3e1677
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-26 20:43:39 -0400

    upload files with git

---
+++ e1.7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+	cout << "They Both Die at the End by Adam Silvera" << endl;
+	cout << "A Thousand Splendid Suns" << endl;
+	cout << "The Song of Achilles" << endl;
+
+	return 0;
+}
