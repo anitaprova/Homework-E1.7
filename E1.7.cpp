365aa9bd827a01738db33d877ef568d7eb3de3bc
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-29 12:14:11 -0400

    added comments

--- E1.7.cpp
+++ E1.7.cpp
@@ -1,3 +1,12 @@
+/*
+Author: Anita Prova
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E1.7
+
+The program prints my favorite books on separate lines. 
+*/
+
 #include <iostream>
 using namespace std;
 
