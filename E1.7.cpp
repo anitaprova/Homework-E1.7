2a91497a69e39f8e8d9bc19122127129f74f26eb
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-08-29 12:14:59 -0400

    updated comment

--- E1.7.cpp
+++ E1.7.cpp
@@ -4,7 +4,7 @@ Course: CSCI-135
 Instructor: Genady Maryash
 Assignment: Homework E1.7
 
-The program prints my favorite books on separate lines. 
+The program prints some names on separate lines. 
 */
 
 #include <iostream>
